3e99e497e2dd19b1129418450a20ee550c194299
Erik Torres <erik.torres@codurance.com>
2017-06-14 10:40:16 +0200

    File employee repository tested

--- src/main/java/com/github/etorres/birthdaygreetings/Message.java
+++ src/main/java/com/github/etorres/birthdaygreetings/Message.java
@@ -1,7 +1,29 @@
 package com.github.etorres.birthdaygreetings;
 
-/**
- * Created by etorres on 14/06/17.
- */
+import java.util.Objects;
+
 public class Message {
+
+    private final String subject;
+    private final String body;
+
+    public Message(String subject, String body) {
+        this.subject = subject;
+        this.body = body;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Message message = (Message) o;
+        return Objects.equals(subject, message.subject) &&
+                Objects.equals(body, message.body);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(subject, body);
+    }
+
 }
---
+++ src/test/java/com/github/etorres/birthdaygreetings/FileEmployeeRepositoryShould.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by etorres on 14/06/17.
+ */
+public class EmployeeRepositoryShould {
+
+}
